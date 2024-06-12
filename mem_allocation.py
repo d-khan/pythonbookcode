8ef4aef509bad6d147c7e4013856b27c0cd65f22
Danish Khan <dkhan1010@gmail.com>
2024-06-12 08:58:42 -0700

    Update mem_allocation.py

--- mem_allocation.py
+++ mem_allocation.py
@@ -1,7 +1,7 @@
 def variable_mem():
     address = set([])
     var1 = 10
-    print("var1 value is", var1, "and the memory address is", hex(id(var1)),end="")
+    print("var1 value is", var1, "and the memory address is", hex(id(var1)), end="")
     if hex(id(var1)) not in address:
         print(" - New memory address")
     else:
@@ -9,7 +9,7 @@ def variable_mem():
 
     address.add(hex(id(var1)))
     var2 = 20
-    print("var2 value is", var2, "and the memory address is", hex(id(var2)),end="")
+    print("var2 value is", var2, "and the memory address is", hex(id(var2)), end="")
     if hex(id(var2)) not in address:
         print(" - New memory address")
     else:
@@ -17,7 +17,7 @@ def variable_mem():
 
     address.add(hex(id(var2)))
     var3 = 10
-    print("var3 value is", var3, "and the memory address is", hex(id(var3)),end="")
+    print("var3 value is", var3, "and the memory address is", hex(id(var3)), end="")
     if hex(id(var3)) not in address:
         print(" - New memory address")
     else:
@@ -25,7 +25,7 @@ def variable_mem():
 
     address.add(hex(id(var3)))
     var4 = 20
-    print("var4 value is", var4, "and the memory address is", hex(id(var4)),end="")
+    print("var4 value is", var4, "and the memory address is", hex(id(var4)), end="")
     if hex(id(var4)) not in address:
         print(" - New memory address")
     else:
@@ -33,8 +33,7 @@ def variable_mem():
 
     address.add(hex(id(var4)))
     var1 = 20
-    print("var1 value is", var1, "and the memory address is", hex(id(var1)),end="")
-    address.add(hex(id(var1)))
+    print("var1 value is", var1, "and the memory address is", hex(id(var1)), end="")
     if hex(id(var1)) not in address:
         print(" - New memory address")
     else:
@@ -42,8 +41,7 @@ def variable_mem():
 
     address.add(hex(id(var1)))
     var1 = 30
-    print("var1 value is", var1, "and the memory address is", hex(id(var1)),end="")
-    address.add(hex(id(var1)))
+    print("var1 value is", var1, "and the memory address is", hex(id(var1)), end="")
     if hex(id(var1)) not in address:
         print(" - New memory address")
     else:
@@ -51,8 +49,7 @@ def variable_mem():
 
     address.add(hex(id(var1)))
     var1 = "Hello"
-    print("var1 value is", var1, "and the memory address is", hex(id(var1)),end="")
-    address.add(hex(id(var1)))
+    print("var1 value is", var1, "and the memory address is", hex(id(var1)), end="")
     if hex(id(var1)) not in address:
         print(" - New memory address")
     else:
@@ -60,11 +57,11 @@ def variable_mem():
 
     address.add(hex(id(var1)))
     var1 = 30
-    print("var1 value is", var1, "and the memory address is", hex(id(var1)),end="")
-    address.add(hex(id(var1)))
+    print("var1 value is", var1, "and the memory address is", hex(id(var1)), end="")
     if hex(id(var1)) not in address:
         print(" - New memory address")
     else:
         print(" - Memory reused")
-    
+
+
 variable_mem()
